e0dd8d45c231e6e150df2ca799bb552bc148ffed
sr-rizquez <SR.RIZQUEZ.96@GMAIL.COM>
2026-03-10 19:29:10 +0100

    Update Datos.java
    Sustitución de ArrayList por HasMap para manejo de la unicidad de mails

--- src/poobdgroup.modelo/Datos.java
+++ src/poobdgroup.modelo/Datos.java
@@ -1,19 +1,20 @@
 package poobdgroup.modelo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Datos {
     private ArrayList<Articulo> articulos = new ArrayList<Articulo>();
-    private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    private HashMap<String, Cliente> clientes = new HashMap<>();
     private ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
 
 
     public Datos() {}
 
 
-    public Datos(ArrayList<Articulo> articulos, ArrayList<Cliente> clientes, ArrayList<Pedido> pedidos) {
+    public Datos(ArrayList<Articulo> articulos, HashMap<String, Cliente> clientes, ArrayList<Pedido> pedidos) {
         this.articulos = (articulos != null) ? articulos : new ArrayList<>();
-        this.clientes = (clientes != null) ? clientes : new ArrayList<>();
+        this.clientes = (clientes != null) ? clientes : new HashMap<>();
         this.pedidos = (pedidos != null) ? pedidos : new ArrayList<>();
     }
 
@@ -22,7 +23,7 @@ public class Datos {
         return articulos;
     }
 
-    public ArrayList<Cliente> getClientes() {
+    public HashMap<String, Cliente> getClientes() {
         return clientes;
     }
 
