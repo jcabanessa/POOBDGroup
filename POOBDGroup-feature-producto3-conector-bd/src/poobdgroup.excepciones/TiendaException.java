e6528d924606a2c6606bdc63ceb8ad52a216bf4c
rromerosant-uoc <rromerosant@uoc.edu>
2026-03-18 22:16:20 +0100

    Limpieza total y estructura correcta del proyecto

--- POOBDGroup-feature-producto3-conector-bd/src/poobdgroup.excepciones/TiendaException.java
+++
@@ -1,8 +0,0 @@
-package poobdgroup.excepciones;
-
-// Excepción genérica para la aplicación
-public class TiendaException extends Exception {
-    public TiendaException(String mensaje) {
-        super(mensaje);
-    }
-}
