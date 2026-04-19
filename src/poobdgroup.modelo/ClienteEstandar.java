9f36ef93984c6af10f8b3dc9bf86c584fe108880
jordikick <jordi_kick87@hotmail.com>
2026-04-19 12:00:39 +0200

    P4_ORM. Cambio de clases en Modelo normales y DAO. Queda igual Controlador y vista. Limpieza de código. Funciona correcto todo

--- src/poobdgroup.modelo/ClienteEstandar.java
+++ src/poobdgroup.modelo/ClienteEstandar.java
@@ -1,17 +1,5 @@
 package poobdgroup.modelo;
 
-/*public class ClienteEstandar extends Cliente{
-
-    public ClienteEstandar(String nombre, String domicilio, String nif, String email) {
-        super(nombre, domicilio, nif, email);
-    }
-
-
-    @Override public String tipoCliente() { return "Estandar"; }
-    @Override public double calcAnual() { return 0; }
-    @Override public double descuentoEnvio() { return 0; }
-}*/
-
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
